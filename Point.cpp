4136c79da9f57dd8c4fb7a2d7f1e9c30a884fff6
orlambda <115938017+orlambda@users.noreply.github.com>
2024-11-19 01:49:40 +0000

    Add Point class

---
+++ Point.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Orlando Shamlou on 19/11/2024.
+//
+
+#include "Point.h"
+
+Point::Point()
+{
+    x = 0;
+    y = 0;
+}
---
+++ Point.h
@@ -0,0 +1,21 @@
+//
+// Created by Orlando Shamlou on 19/11/2024.
+//
+
+#ifndef POINT_H
+#define POINT_H
+
+
+// A struct might be fine?
+// All public, defaults are 0
+// Could use std::pair?
+// Keep related functions in class or in a namespace?
+class Point {
+public:
+    double x;
+    double y;
+    Point();
+};
+
+
+#endif //POINT_H
